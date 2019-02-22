57aa696febf46acaabc66b92a9fb1a87464f3250
Admin <xufangbo@hotmail.com>
2019-02-22 21:59:16 +0800

    xx

---
+++ js/English.js/kaixin.js
@@ -0,0 +1,6 @@
+var a=3+4;
+console.log(a)
+console.log("hello")
+hello=3
+
+k=5
---
+++ js/number.js/00.js
@@ -0,0 +1,26 @@
+function hello() {
+    console.log("hello")
+    for (i = 0; i < 10; i++) {
+        console.log("zzzzzzzzzzzzzzzzzz");
+        console.log("xxxxxxxxxxxxxxxxx");
+        console.log("yyyyyyyyyyyyyyyyy");
+    }
+    console.log("11111111111111")
+}
+function hello() {
+    console.log("hello")
+    for (i = 0; i < 10; i++) {
+        console.log("zzzzzzzzzzzzzzzzzz");
+        console.log("xxxxxxxxxxxxxxxxx");
+        console.log("yyyyyyyyyyyyyyyyy");
+    }
+    console.log("11111111111111")
+}
+
+// hello ("ffffffffffff");
+
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")

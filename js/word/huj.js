8c61dbb2041d292e4c89681b8cc307f07846c932
kaixin2012 <xuxingzhi@aliyun.com>
2020-02-24 18:29:21 +0800

    xx

--- js/word/huj.js
+++ js/word/huj.js
@@ -1,13 +1,13 @@
-function prime(num){
-    for(var i=2;i<num;i++){
-        var y=num%i;
-        if(y==0){
-            return false;
-        }
-    }
-    return true;
+function prime(num) {
+	for (var i = 2; i < num; i++) {
+		var y = num % i;
+		if (y == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
-var h=1000;
-var e33= prime(h);
-console.log("prime is "+e33);
+var h = 1000;
+var e = prime(h);
+console.log("prime is " + e);

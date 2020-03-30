f8e7a538fdfa738caab55b46e21aa7e471b1fd00
Admin <xufangbo@hotmail.com>
2020-03-30 19:56:36 +0800

    修改说明

--- nodejs/server1.js
+++ nodejs/server1.js
@@ -32,4 +32,4 @@ http.createServer(function (request, response) {
     }
 }).listen(8889);
 
-console.log('server running at http://127.0.0.1.8889');
+console.log('server running at http://127.0.0.1:8889');

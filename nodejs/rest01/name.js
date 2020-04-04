ab6f216a432dedd63eb69c4819a31c993456fb40
Admin <xufangbo@hotmail.com>
2020-04-04 11:12:08 +0800

    修改说明

--- nodejs/rest01/name.js
+++ nodejs/rest01/name.js
@@ -1,12 +1,10 @@
-var user1 = { "name": "徐行知", "id": 1, "age": 8 };
-var user2 = { "name": "马义晨", "id": 2, "age": 10 };
-var user3 = { "name": "商浩轩", "id": 3, "age": 10 };
+var user1 = {"name" : "徐行知","id" : 1,"age" : 8};
+var user3 = {"name" : "马义晨","id" : 2,"age" : 10};
+var user2 = {"name" : "商浩轩","id" : 3,"age" : 10};
 
-var users = [user1, user2, user3];
+var users = [user1,user2,user3]
 
-for (i = 0; i < users.length; i++) {
-
+for (var i = 0;i<=(users.length-1);i++){
     var user = users[i];
-
-    console.log(user.name);
+    console.log(user.id);
 }
